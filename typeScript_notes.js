236817d809e1bbb6fc242f2000a40fb33d2db10b
nifu <nifu@paypal.com>
2017-02-09 00:15:37 -0800

    add notes

---
+++ typeScript_notes.js
@@ -0,0 +1,5 @@
+// import
+import { myOtherComponent } from 'myOther.component';   //no   .ts at the end
+
+//Decorators
+@component() //takes a javascript object as an argument and add some metadata (do somethign in the background). That makes this class a Component which Angular2 can recognize.
